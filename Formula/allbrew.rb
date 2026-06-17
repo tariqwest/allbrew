1908727b83c532f5872f7258083b0e95174e1311
Tariq West <tariqwest@gmail.com>
2026-06-17 18:40:50 -0400

    chore: add Homebrew release workflow

--- Formula/allbrew.rb
+++ Formula/allbrew.rb
@@ -1,15 +1,25 @@
 class Allbrew < Formula
   desc "Generate Homebrew formulas and casks from arbitrary URLs"
   homepage "https://github.com/tariqwest/homebrew-allbrew"
-  url "https://github.com/tariqwest/homebrew-allbrew/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/tariqwest/homebrew-allbrew/releases/download/v1.0.0/allbrew-v1.0.0.tar.gz"
   sha256 "PLACEHOLDER"
   license "MIT"
 
-  depends_on "node"
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "bun"
 
   def install
-    system "npm", "install", *std_npm_args
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    libexec.install Dir["*"]
+
+    (bin/"allbrew").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["bun"].opt_bin}/bun" "#{libexec}/bin/allbrew.ts" "$@"
+    EOS
+    chmod 0755, bin/"allbrew"
   end
 
   test do
